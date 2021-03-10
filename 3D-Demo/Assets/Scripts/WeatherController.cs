0ddd40a4788a6624eb8623db019ce6afda8f25cc
Iván Efraim Balázs <iebalazs@gmail.com>
2021-03-10 12:34:35 +0100

    Move 3D-Demo to github

---
+++ 3D-Demo/Assets/Scripts/WeatherController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherController : MonoBehaviour
+{
+    [SerializeField] private Material sky;
+    [SerializeField] private Light sun;
+
+    private float _fullIntensity;
+    private float _sunnyValue = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _fullIntensity = sun.intensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_sunnyValue > 0) {
+            SetOvercast(_sunnyValue);
+            _sunnyValue -= 0.0005f;
+        }
+    }
+
+    private void SetOvercast(float value) {
+        sky.SetFloat("_Blend", value);
+        sun.intensity = 0.6f + 0.4f * _fullIntensity * value;
+    }
+}
